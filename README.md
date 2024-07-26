4ba93f7b8e7974551a6fafe4abc738102040b938
Florian LASSONT <flassont@betomorrow.com>
2024-07-26 17:45:36 +0200

    Import initial plugin version

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# expo-android-release-config
+# @betomorrow/expo-android-release-config
+
+Provide Android signing release of your Expo CNG app from file.
+
+# Installation in managed Expo projects
+
+Add this package to your dependencies and reference it in the `plugins` section of your `app.json`/`app.config.js`/`app.config.ts`:
+```json
+{
+    "plugins": [
+        "@betomorrow/expo-android-release-config",
+    ]
+}
+```
+No extra configuration can be passed to the plugin at the moment.
+
+Rerun `npx expo prebuild --clean` for the plugin to apply.
+
+Plugin expect a file located at `android/signing.properties` (from your Expo app root), whose keys are described in the [example file](./examples/signing.properties).
+
+# Installation in bare React Native projects
+
+Installing this package in a bare React Native project is pointless since you can put your own signing config and commit it.
+
+### Add the package to your npm dependencies
+
+```
+npm install expo-android-release-config
+```
+
+
+
+
+# Contributing
+
+Contributions are very welcome! Please refer to guidelines described in the [contributing guide]( https://github.com/expo/expo#contributing).
---
+++ plugin/src/index.ts
@@ -0,0 +1,41 @@
+// @ts-check
+import { withAppBuildGradle, type ConfigPlugin } from "@expo/config-plugins";
+
+const androidSigningConfig = `
+android {
+    defaultConfig {
+        versionCode project.hasProperty("versionCode") ? project.property("versionCode") as Integer : 1
+    }
+    signingConfigs {
+        release {
+            file("../signing.properties").with { propFile ->
+                if (propFile.canRead()) {
+                    def properties = new Properties()
+                    properties.load(new FileInputStream(propFile))
+                    storeFile file(properties['keystorePath'])
+                    storePassword properties['keystorePassword']
+                    keyAlias properties['keyAlias']
+                    keyPassword properties['keyPassword']
+                } else {
+                    println 'Unable to read signing.properties'
+                }
+            }
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig signingConfigs.release
+        }
+    }
+}
+`;
+
+export interface PluginProps {}
+
+const withAndroidSigningInjection: ConfigPlugin<PluginProps> = (config) =>
+  withAppBuildGradle(config, async (file) => {
+    file.modResults.contents += androidSigningConfig;
+    return file;
+  });
+
+export default withAndroidSigningInjection;
